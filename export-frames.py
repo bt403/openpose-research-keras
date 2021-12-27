dde114cfb51fcd8a6eccf372769b551bc864d59d
Bruno T <bruno@Brunos-MacBook-Pro.local>
2021-12-27 00:10:55 +0000

    generate frames script

--- export-frames.py
+++ export-frames.py
@@ -185,21 +185,21 @@ for ivid,vid in enumerate(videos):
 
         for index in tqdm(range(nframes)):
             input_image = clip.load_frame()
-            second = index/fps
+            second = math.floor(index/fps,0)
             frame_number = 1
             try:
                 real_frame_number = index
                 filename = "/content/drive/MyDrive/ResearchProject/videos-bright-6/images-batch-1/" + vidname + "_" + str(real_frame_number) + "_" + str(frame_number) + ".jpg"
-                if (index > second and index <= second + 1 ):
-                    cv2.imwrite(filename, input_image)
+                if (index/fps > second and index/fps <= second + 1 ):
+                    cv2.imwrite(filename, cv2.cvtColor(input_image, cv2.COLOR_RGB2BGR))
                     print(filename)
                     frame_number +=1
-                elif (index > second + fps/3 and index <= second + fps/3 + 1):
-                    cv2.imwrite(filename, input_image)
+                elif (index/fps > second + fps/3 and index/fps <= second + fps/3 + 1):
+                    cv2.imwrite(filename, cv2.cvtColor(input_image, cv2.COLOR_RGB2BGR))
                     print(filename)
                     frame_number += 1
-                elif (index > second + fps/3*2 and index <= second + fps/3*2 + 1):
-                    cv2.imwrite(filename, input_image)
+                elif (index/fps > second + fps/3*2 and index/fps <= second + fps/3*2 + 1):
+                    cv2.imwrite(filename, cv2.cvtColor(input_image, cv2.COLOR_RGB2BGR))
                     print(filename)
                     frame_number += 1
             except Exception as e:
