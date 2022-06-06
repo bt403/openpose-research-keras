2f515a2d2b9494b9ac077a1aa3f8a0f26751763c
Bruno T <bruno@dhcp-10-248-108-77.eduroam.wireless.private.cam.ac.uk>
2022-06-06 05:05:46 +0100

    export features

--- utils_feature_extraction.py
+++ utils_feature_extraction.py
@@ -8,6 +8,7 @@ import mediapipe as mp
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
 
 def removeOutliers(group):
   c = 0
