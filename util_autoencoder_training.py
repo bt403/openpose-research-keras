dd50cd5f2ac6b5c4cf01eef0cf5dfa03794f4abe
Bruno T <bruno@dhcp-10-248-108-77.eduroam.wireless.private.cam.ac.uk>
2022-06-06 09:41:38 +0100

    preprocess, and classify targets

--- util_autoencoder_training.py
+++ util_autoencoder_training.py
@@ -98,7 +98,7 @@ def runCrossValidationAutoencoderSVM_withHOG(results, x_data, x_data_hog, x_data
     x_train = model_encoder([np.array(x_train_2), np.array(x_train_hog)])[2]
     x_test = model_encoder([np.array(x_test_2), np.array(x_test_hog)])[2]
 
-    params = getBestParams(x_train.numpy(), x_data[target_val], with_pca=False, binary=binary, groups=x_data["video_name"])
+    params = getBestParams(x_train.numpy(), x_data[target_val], with_pca=False, binary=binary, ) #groups=x_data["video_name"])
     #train_svm(results, x_train.numpy(), x_data[target_val], x_test, x_data_test[target_val], model_name, params, x_data["video_name"], weights, False, binary)
     train_svm(results, x_train.numpy(), x_data[target_val], x_test, x_data_test[target_val], model_name, params, None, weights, False, binary)
 
